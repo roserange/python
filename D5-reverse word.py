51fa688a6b6701cd2da281fd8a060fe39fd49ae5
roserange <124559822+roserange@users.noreply.github.com>
2023-02-06 14:27:49 +0530

    Add files via upload

---
+++ D5-reverse word.py
@@ -0,0 +1,5 @@
+def reverse_string_words(text):
+    for line in text.split('\n'):
+        return(' '.join(line.split()[::-1]))
+print(reverse_string_words("The quick brown fox jumps over the lazy dog."))
+print(reverse_string_words("Python Exercises."))
